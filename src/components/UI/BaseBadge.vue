dd21dd08002752ad1029b35eaa65a4d91178e415
V1R4R <welenc09@gmail.com>
2025-09-03 11:58:59 +0200

    add new UI elements

---
+++ src/components/UI/BaseBadge.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
+
+.frontend {
+  background-color: #48c413;
+  color: white;
+}
+
+.backend {
+  background-color: #8f2e4a;
+  color: white;
+}
+
+.career {
+  background-color: #eb7a18;
+  color: white;
+}
+</style>
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,73 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :class="mode" :to="to">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    mode: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #5353bc;
+  border: 1px solid #5353bc;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #3636bf;
+  border-color: #3636bf;
+}
+
+.flat {
+  background-color: transparent;
+  color: #5353bc;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #4646bf;
+  color: #4646bf;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #d2d2f9;
+}
+</style>
