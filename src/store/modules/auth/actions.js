a179c8b9dc26de9b16b70412a7e9c639dcb31526
V1R4R <welenc09@gmail.com>
2025-09-04 15:44:31 +0200

    login and signup refactor

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,49 +1,37 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_GOOGLE_AUTH_API_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_GOOGLE_AUTH_API_KEY}`;
+
+    if (mode === 'signup') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_GOOGLE_AUTH_API_KEY}`;
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = response.json();
 
     if (!response.ok) {
       const error = new Error(responseData.message || 'Failed to login!');
-      throw error;
-    }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
-  },
-  async signup(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_GOOGLE_AUTH_API_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = response.json();
-
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to authenticate!'
-      );
       throw error;
     }
 
